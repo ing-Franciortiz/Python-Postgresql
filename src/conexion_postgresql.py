a04c2da9a79f2a772d259a8407722c26a9af7f1b
ing-Franciortiz <franciortiz2004@icloud.com>
2025-10-13 17:13:53 -0400

    Primer commit: conexión Python con PostgreSQL

---
+++ src/conexion_postgresql.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+
+   
+connection = psycopg2.connect(host='localhost',user='postgres',password='Franci2025',port=5432,database='test_db')
+
+
+try:
+    with connection:
+        with connection.cursor() as cursor:
+           sentencia = "INSERT INTO Persona(nombre, apellido, email) VALUES(%s, %s, %s)"
+           valores = (
+              ("Carlos", "Lara", "clara@.gmail.com"),
+              ("Jose", "Manuel", "jmanuel@gmail.com"),
+              ("marcos", "Cantu", "cmarcos@gamil.com")
+           )
+           cursor.executemany(sentencia, valores)
+           registros_insertados = cursor.rowcount
+           print(f"Registros Insertados: {registros_insertados}")
+
+except Exception as e:
+    print(f"Ocurrio un error {e}")
+finally:
+    connection.close()
