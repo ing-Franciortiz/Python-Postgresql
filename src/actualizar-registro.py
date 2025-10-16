c1953921026f7b31c92897716aae1c1fa97c8b76
ing-Franciortiz <franciortiz2004@icloud.com>
2025-10-15 20:17:30 -0400

    actualizar-registro

---
+++ src/actualizar-registro.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+
+   
+connection = psycopg2.connect(host='localhost',user='postgres',password='Franci2025',port=5432,database='test_db')
+
+
+try:
+    with connection:
+        with connection.cursor() as cursor:
+            sentencia = "UPDATE Persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s"
+            valores = ("Juan Carlos", "Juarez", "jcjuarez@gmail.com", 1)
+            cursor.execute(sentencia, valores)
+            registro_actualizados = cursor.rowcount
+            print(f"Registros Actualizados: {registro_actualizados}")
+except Exception as e:
+    print(f"Ocurrio un error {e}")
+finally:
+    connection.close()
